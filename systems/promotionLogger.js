91f17807a54bb74ff4d2e2c2c6a57c61f2889bc0
idkkkbor <tammineneino3@gmail.com>
2026-08-10 21:03:34 +0300

    Add promotion logger functionality

---
+++ systems/promotionLogger.js
@@ -0,0 +1,14 @@
+const { EmbedBuilder } = require("discord.js");
+
+function startPromotionLogger(client) {
+
+    console.log("Promotion logger started.");
+
+    // Promotion logger will be connected to the SGH
+    // Maple Hospital promotion webhook here.
+
+}
+
+module.exports = {
+    startPromotionLogger
+};
